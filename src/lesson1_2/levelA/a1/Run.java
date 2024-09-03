1f8ff7fc13daf69d4ef444e446e9595836019b6f
ManaviBLR <axad130427@gmail.com>
2024-09-03 14:12:13 +0300

    задача1

---
+++ src/lesson1_2/levelA/a1/Run.java
@@ -0,0 +1,30 @@
+package lesson1_2.levelA.a1;
+/*
+Объявление и инициализация переменных.  В методе main класса Run создайте 8 переменных каждого примитивного типа
+ и присвойте им любые произвольные значения. Напишите код, который будет выводить значение каждой
+ переменной в консоль.
+ */
+public class Run {
+    public static void main(String []args) {
+        int x = 10;
+        boolean y = false;
+        char d = 90;
+        byte c = 2;
+        short v = 1;
+        long b = 2_000_000_000_000L;
+        float n = 3;
+        double m = 1.5;
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(v);
+        System.out.println(b);
+        System.out.println(n);
+        System.out.println(m);
+
+
+
+    }
+}
+
---
+++ src/lesson1_2/levelB/b1/Run.java
@@ -0,0 +1,4 @@
+package lesson1_2.levelB.b1;
+
+public class Run {
+}
